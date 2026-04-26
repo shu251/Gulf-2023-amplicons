3a9232f384d63a2c4c387f35e45a862935c63752
Sarah Hu <skhu@tamu.edu>
2026-04-26 09:56:24 -0500

    revision to results

--- README.md
+++
@@ -1,6 +0,0 @@
-Northern Gulf cruise data analysis (2023)
-
-Hu lab
-
-See site for compiled code: https://shu251.github.io/Gulf-2023-amplicons/
-
---
+++ fxn-runRDA_1000_HPRC.R
@@ -0,0 +1,297 @@
+# Function to run highly robust RDA and ANOVA analysis for Gulf data
+library(tidyverse)
+library(compositions)
+library(vegan)
+
+load(file = "/scratch/group/hu-lab/GoM-amplicon-analysis/GoM-r-outputs/RDA_Robjects_02072026.RData", verbose = TRUE)
+
+
+set.seed(10234)
+
+# Function to create dataframes from output
+run_rda <- function(mod_metadata, mod_asv_long, 
+                    DATASET, features = c("include", "exclude")){
+  # Select categorical data
+  # mod_metadata <- surf_table
+  # mod_asv_long <- surf_asv
+  if (features == "include") {
+    categorical <- mod_metadata %>% 
+      select(STN_NISKIN, STN_ORDER, TRANSECT, 
+             OilRig_Count, DAY_NIGHT, ends_with("_THRESHOLD")) %>% 
+      distinct()
+  } else {
+    categorical <- mod_metadata %>% 
+      select(STN_NISKIN, STN_ORDER, TRANSECT, 
+             OilRig_Count, DAY_NIGHT) %>% distinct()}
+  #
+  # Isolate numerical data as matrix
+  metadata_for_rda_0 <- mod_metadata %>% 
+    select(STN_NISKIN, Depth, DIST_OUTFLOW, NH4, NO2, NO3, Oxygen, PO4, SIL, Salinity, Temperature) %>% distinct() %>% 
+    pivot_longer(cols = -c(STN_NISKIN), names_to = "VARIABLES", values_to = "VALUE") %>% 
+    group_by(STN_NISKIN, VARIABLES) %>% 
+    summarise(MEAN_VAL = mean(VALUE)) %>% 
+    pivot_wider(names_from = VARIABLES, values_from = MEAN_VAL) %>% 
+    arrange(STN_NISKIN) %>% 
+    column_to_rownames(var = "STN_NISKIN") %>% as.matrix
+  # Normalize
+  standarize_metadata <- decostand(metadata_for_rda_0, MARGIN = 2, method = "standardize")
+  # Combine normalized numerical data with categorical
+  metadata_normalized <- categorical %>% 
+    right_join(as.data.frame(standarize_metadata) %>% rownames_to_column(var = "STN_NISKIN")) %>%
+    arrange(STN_NISKIN) %>% 
+    column_to_rownames(var = "STN_NISKIN")
+  #
+  ## Prepare ASV data
+  asv_wide_avg_reps_matrix <- mod_asv_long %>% 
+    ungroup() %>%  
+    select(FeatureID, STN_NISKIN, MEAN_REPS_seq) %>% 
+    pivot_wider(names_from = STN_NISKIN, values_from = MEAN_REPS_seq, values_fn = mean, values_fill = 0) %>% 
+    column_to_rownames(var = "FeatureID") %>% 
+    as.matrix()
+  # Transform
+  asv_wide_avg_rda_0 <- data.frame(t(asv_wide_avg_reps_matrix)) %>% 
+    rownames_to_column(var = "STN_NISKIN") %>% 
+    arrange(STN_NISKIN) %>% 
+    column_to_rownames(var = "STN_NISKIN") %>% 
+    as.matrix
+  #
+  # CLR normalization
+  clr_asv_matrix <- data.frame(compositions::clr(asv_wide_avg_rda_0))
+  #
+  # setdiff(rownames(metadata_normalized), (rownames(clr_asv_matrix)))
+  # RDA step
+  rda_output <- rda(clr_asv_matrix ~ ., data = metadata_normalized, na.action = na.exclude)
+  # rda_output
+  #
+  # R-squared value
+  adj_rda_out <- RsquareAdj(rda_output)$adj.r.squared #adj_rda_out
+  # Run ANOVA
+  anova_out <- anova.cca(rda_output, step = 100, by = "term")
+  #
+  # Get stats
+  ## Percentage constrained
+  total_amount <- (rda_output$tot.chi)
+  constrained <- (rda_output$CCA$tot.chi)
+  perc_constratained <- constrained/total_amount
+  # Formula
+  formula <- as.character(rda_output$call)[2]
+  # Create data frame output for RDAs run
+  rda_results_df <- data.frame(scores(anova_out)) %>% 
+    rownames_to_column(var = "VARIABLES") %>% 
+    select(VARIABLES, P_VALUE = `Pr..F.`) %>% 
+    add_column(TEST = DATASET,
+               PERC_CONSTRAINED = perc_constratained,
+               ADJ_R_SQUARED = adj_rda_out,
+               FORMULA = formula)
+  #
+  return(rda_results_df)
+}
+
+##
+# All
+##
+all_samples <- table_supp %>% ungroup()
+all_samples_list <- as.character(unique(table_supp$STN_NISKIN))
+asvs_all_samples <- asv_long_avg_wtax_clean %>%
+  filter(STN_NISKIN %in% all_samples_list) %>% ungroup()
+
+all_rda <- run_rda(all_samples, asvs_all_samples,
+                   "All samples", features = "include")
+# all_rda
+##
+# Above/Below DCM
+##
+surf_table <- table_supp %>% 
+  filter(grepl("Above",DCM_THRESHOLD)) %>% 
+  ungroup()
+surf_subset <- as.character(unique(surf_table$STN_NISKIN))
+surf_asv <- asv_long_avg_wtax_clean %>%
+  filter(STN_NISKIN %in% surf_subset) %>% ungroup()
+above_dcm_rda <- run_rda(surf_table, surf_asv, "Above DCM", features = "exclude")
+
+below_table <- table_supp %>% 
+  filter(!(grepl("Above",DCM_THRESHOLD))) %>% 
+  ungroup()
+below_subset <- as.character(unique(below_table$STN_NISKIN))
+below_asv <- asv_long_avg_wtax_clean %>%
+  filter(STN_NISKIN %in% below_subset) %>% ungroup()
+below_dcm_rda <- run_rda(below_table, below_asv, "Below DCM", features = "exclude")
+# below_dcm_rda
+
+##
+# Above/Below O2 minimum
+below_02_table <- table_supp %>% 
+  filter(!(grepl("Above",DCM_THRESHOLD))) %>% 
+  ungroup()
+below_02_subset <- as.character(unique(below_02_table$STN_NISKIN))
+below_02_asv <- asv_long_avg_wtax_clean %>% 
+  filter(STN_NISKIN %in% below_02_subset) %>% ungroup()
+below_02_rda <- run_rda(below_02_table, below_02_asv, "Below O2", features = "exclude")
+# below_02_rda
+
+above_02_table <- table_supp %>% 
+  filter((grepl("Above",DCM_THRESHOLD))) %>% 
+  ungroup()
+above_02_subset <- as.character(unique(above_02_table$STN_NISKIN))
+above_02_asv <- asv_long_avg_wtax_clean %>%
+  filter(STN_NISKIN %in% above_02_subset) %>% ungroup()
+above_02_rda <- run_rda(above_02_table, above_02_asv, "Above O2", features = "exclude")
+# above_02_rda
+
+##
+# Above/Below MLD
+below_MLD_table <- table_supp %>% 
+  filter(!(grepl("Above", MLD_THRESHOLD))) %>% 
+  ungroup()
+below_MLD_subset <- as.character(unique(below_MLD_table$STN_NISKIN))
+below_MLD_asv <- asv_long_avg_wtax_clean %>% 
+  filter(STN_NISKIN %in% below_MLD_subset) %>% ungroup()
+below_MLD_rda <- run_rda(below_MLD_table, below_MLD_asv, "Below MLD", features = "exclude")
+# below_MLD_rda
+
+above_MLD_table <- table_supp %>% 
+  filter((grepl("Above", MLD_THRESHOLD))) %>% 
+  ungroup()
+above_MLD_subset <- as.character(unique(above_MLD_table$STN_NISKIN))
+above_MLD_asv <- asv_long_avg_wtax_clean %>% 
+  filter(STN_NISKIN %in% above_MLD_subset) %>% ungroup()
+above_MLD_rda <- run_rda(above_MLD_table, above_MLD_asv, "Above MLD", features = "exclude")
+# below_MLD_rda
+
+##
+# Dinoflagellates
+dino_asv <- asv_long_avg_wtax_clean %>%
+  filter(Supergroup_simplified == "TSAR-Dinoflagellata") %>% 
+  filter(Class != "Syndiniales") %>%
+  ungroup()
+
+dino_rda <- run_rda(table_supp, dino_asv, "All dinoflagellates (no Syn)", features = "include")
+# dino_rda
+
+aboveMLD_dino_asv <- dino_asv %>% 
+  filter(STN_NISKIN %in% above_MLD_subset) %>% ungroup()
+dino_rda_aboveMLD <- run_rda(above_MLD_table, aboveMLD_dino_asv, "Dinoflagellates (no Syn) above MLD", features = "exclude")
+
+belowMLD_dino_asv <- dino_asv %>% 
+  filter(STN_NISKIN %in% below_MLD_subset) %>% ungroup()
+dino_rda_belowMLD <- run_rda(below_MLD_table, belowMLD_dino_asv, "Dinoflagellates (no Syn) below MLD", features = "exclude")
+
+
+## 
+# Syndiniales
+syn_asv <- asv_long_avg_wtax_clean %>%
+  filter(Class == "Syndiniales") %>% 
+  ungroup()
+
+syndiniales_rda <- run_rda(table_supp, syn_asv, "Syndiniales", features = "include")
+
+aboveMLD_syn_asv <- syn_asv %>% 
+  filter(STN_NISKIN %in% above_MLD_subset) %>% ungroup()
+syn_rda_aboveMLD <- run_rda(above_MLD_table, aboveMLD_syn_asv, "Syndiniales above MLD", features = "exclude")
+
+belowMLD_syn_asv <- syn_asv %>% 
+  filter(STN_NISKIN %in% below_MLD_subset) %>% ungroup()
+syn_rda_belowMLD <- run_rda(below_MLD_table, belowMLD_syn_asv, "Syndiniales below MLD", features = "exclude")
+
+##
+# Radiolaria
+rad_asv <- asv_long_avg_wtax_clean %>%
+  filter(Supergroup_simplified == "TSAR-Radiolaria") %>% 
+  ungroup()
+
+rad_rda <- run_rda(table_supp, rad_asv, "Radiolaria", features = "include")
+
+aboveMLD_rad_asv <- rad_asv %>% 
+  filter(STN_NISKIN %in% above_MLD_subset) %>% ungroup()
+rad_rda_aboveMLD <- run_rda(above_MLD_table, aboveMLD_rad_asv, "Radiolaria above MLD", features = "exclude")
+
+belowMLD_rad_asv <- rad_asv %>% 
+  filter(STN_NISKIN %in% below_MLD_subset) %>% ungroup()
+rad_rda_belowMLD <- run_rda(below_MLD_table, belowMLD_rad_asv, "Radiolaria below MLD", features = "exclude")
+
+##
+# Ciliate
+cil_asv <- asv_long_avg_wtax_clean %>%
+  filter(Supergroup_simplified == "TSAR-Ciliophora") %>% 
+ungroup()
+
+cil_rda <- run_rda(table_supp, cil_asv, "Ciliate", features = "include")
+
+aboveMLD_cil_asv <- cil_asv %>% 
+  filter(STN_NISKIN %in% above_MLD_subset) %>% ungroup()
+cil_rda_aboveMLD <- run_rda(above_MLD_table, aboveMLD_cil_asv, "Ciliate above MLD", features = "exclude")
+
+belowMLD_cil_asv <- cil_asv %>% 
+  filter(STN_NISKIN %in% below_MLD_subset) %>% ungroup()
+cil_rda_belowMLD <- run_rda(below_MLD_table, belowMLD_cil_asv, "Ciliate below MLD", features = "exclude")
+
+# Gyrista:
+gyr_asv <- asv_long_avg_wtax_clean %>%
+  filter(Supergroup_simplified == "TSAR-Gyrista") %>%
+  ungroup()
+
+gyr_rda <- run_rda(table_supp, gyr_asv, "Gyrista", features = "include")
+
+aboveMLD_gyr_asv <- gyr_asv %>% 
+  filter(STN_NISKIN %in% above_MLD_subset) %>% ungroup()
+gyr_rda_aboveMLD <- run_rda(above_MLD_table, aboveMLD_gyr_asv, "Gyrista above MLD", features = "exclude")
+
+belowMLD_gyr_asv <- gyr_asv %>% 
+  filter(STN_NISKIN %in% below_MLD_subset) %>% ungroup()
+gyr_rda_belowMLD <- run_rda(below_MLD_table, belowMLD_gyr_asv, "Gyrista below MLD", features = "exclude")
+# Class == "Mediophyceae"
+# Class == "Bacillariophyceae"
+
+#
+# Bigyra:
+bigy_asv <- asv_long_avg_wtax_clean %>%
+  filter(Supergroup_simplified == "TSAR-Bigyra") %>% 
+  ungroup()
+big_rda <- run_rda(table_supp, bigy_asv, "Bigyra", features = "include")
+
+aboveMLD_big_asv <- bigy_asv %>% 
+  filter(STN_NISKIN %in% above_MLD_subset) %>% ungroup()
+big_rda_aboveMLD <- run_rda(above_MLD_table, aboveMLD_big_asv, "Bigyra above MLD", features = "exclude")
+
+belowMLD_big_asv <- bigy_asv %>% 
+  filter(STN_NISKIN %in% below_MLD_subset) %>% ungroup()
+big_rda_belowMLD <- run_rda(below_MLD_table, belowMLD_big_asv, "Bigyra below MLD", features = "exclude")
+
+# 
+# Haptista
+hap_asv <- asv_long_avg_wtax_clean %>%
+  filter(Supergroup_simplified == "Haptista") %>%
+  ungroup()
+hap_subset <- as.character(unique(hap_asv$STN_NISKIN))
+hap_table <- table_supp %>% 
+  filter(STN_NISKIN %in% hap_subset)
+hap_rda <- run_rda(hap_table, hap_asv, "Haptista", features = "include")
+
+aboveMLD_hap_asv <- hap_asv %>% 
+  filter(STN_NISKIN %in% above_MLD_subset) %>% ungroup()
+hap_subset <- as.character(unique(aboveMLD_hap_asv$STN_NISKIN))
+hap_table <- table_supp %>% 
+  filter(STN_NISKIN %in% hap_subset)
+hap_rda_aboveMLD <- run_rda(hap_table, aboveMLD_hap_asv, "Haptista above MLD", features = "exclude")
+
+belowMLD_hap_asv <- hap_asv %>% 
+  filter(STN_NISKIN %in% below_MLD_subset) %>% ungroup()
+hap_subset <- as.character(unique(belowMLD_hap_asv$STN_NISKIN))
+hap_table <- table_supp %>% 
+  filter(STN_NISKIN %in% hap_subset)
+hap_rda_belowMLD <- run_rda(hap_table, belowMLD_hap_asv, "Haptista below MLD", features = "exclude")
+
+# Compile all
+
+df_rda_results <- rbind(all_rda, above_dcm_rda, below_dcm_rda, 
+                        above_02_rda, below_02_rda, above_MLD_rda, below_MLD_rda, 
+                        dino_rda, dino_rda_aboveMLD, dino_rda_belowMLD,
+                        syndiniales_rda, syn_rda_aboveMLD, syn_rda_belowMLD, 
+                        rad_rda, rad_rda_aboveMLD, rad_rda_belowMLD, 
+                        cil_rda, cil_rda_aboveMLD, cil_rda_belowMLD, 
+                        gyr_rda, gyr_rda_aboveMLD, gyr_rda_belowMLD,
+                        big_rda, big_rda_aboveMLD, big_rda_belowMLD,
+                        hap_rda, hap_rda_aboveMLD, hap_rda_belowMLD) %>% 
+  pivot_wider(names_from = VARIABLES, values_from = P_VALUE, values_fill = NA)
+
+save(df_rda_results, file = "/scratch/group/hu-lab/GoM-amplicon-analysis/GoM-r-outputs/df_rda_output_0207202.RData")
